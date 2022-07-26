f1d6b871e828bf36a2c3d815264f2b1dac7ab0bc
Veromadinina <verotldream@gmail.com>
2022-07-26 14:34:54 +0100

    projet youtube

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View,Icon } from 'react-native'
+import React from 'react'
+import Icon2 from 'react-native-vector-icons/AntDesign'
+
+const App = () => {
+  return (
+    <View>
+
+      
+      <Text>salut</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
